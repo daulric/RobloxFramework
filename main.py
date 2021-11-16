863a97111f0e250d64c1c527206fc57714962bad
Ulric <57965846+daulric@users.noreply.github.com>
2021-11-16 16:22:57 -0400

    i have added a bot file

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import discord
+from discord.ext import commands
 
+Prefix = "!"
+bot = commands.Bot(command_prefix=Prefix)
+token = ""
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send("hello")
+
+@bot.command()
+async def on_message(message):
+    if message.startswith("Hello"):
+        await message.send("Hello")
+
+bot.run(token)
